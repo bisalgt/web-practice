0b8c6079faedc5cda0039761812f45ced13e6549
Bishal Gautam <bisalgt@gmail.com>
2020-03-30 23:08:50 +0545

    packages installed with using pip and pipenv

--- apps/webapp/views.py
+++ apps/webapp/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+from inltk.inltk import get_sentence_similarity
+from nltk.corpus import wordnet
+from nltk.tokenize import word_tokenize
+import pandas as pd
+import spacy
+import string
 
 # Create your views here.
 
 def home(request):
-	return HttpResponse("<h1>Home Page!</h1>")
+	url = 'http://promech:6p{f(SDz>7$E9JJd@35.154.93.222/api/v1.1/quiz/?subject_id=47&per_page=54&page=1'
+	r = requests.get(url).json()
+	# print(dir(r))
+	# print(r)
+	# print(len(r))
+	total_number_of_question = len(r["data"])
+	question_listed = list()
+	for i in range(total_number_of_question):
+		question_listed.append(r["data"][i]["question"])
+	# print(question_listed)
+
+	return render(request, 'index.html')
